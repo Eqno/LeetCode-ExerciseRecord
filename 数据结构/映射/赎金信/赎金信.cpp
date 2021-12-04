2346c5cd9dc526117bf7cb5f2a9de0a687cda734
Eqnoxx <Eqnoxx@163.com>
2021-12-04 15:27:58 +0800

    1204

---
+++ 数据结构/映射/赎金信/赎金信.cpp
@@ -0,0 +1,25 @@
+/*
+problem:
+    为了不在赎金信中暴露字迹，从杂志上搜索各个需要的字母，组成单词来表达意思。
+    给你一个赎金信 (ransomNote) 字符串和一个杂志(magazine)字符串，判断 ransomNote 能不能由 magazines 里面的字符构成。
+    如果可以构成，返回 true ；否则返回 false 。
+    magazine 中的每个字符只能在 ransomNote 中使用一次。
+example:
+    输入：ransomNote = "a", magazine = "b"
+    输出：false
+    输入：ransomNote = "aa", magazine = "ab"
+    输出：false
+    输入：ransomNote = "aa", magazine = "aab"
+    输出：true
+*/
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int m[128] = {0};
+        for (auto i: magazine) m[i] ++;
+        for (auto i: ransomNote)
+            if (m[i] > 0) m[i] --;
+            else return false;
+        return true;
+    }
+};
